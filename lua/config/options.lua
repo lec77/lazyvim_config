35dfc2b9df94ec53ebab6746872bdb4163e10904
lec <lecself@163.com>
2024-04-25 22:21:17 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local set = vim.opt
+
+set.shiftwidth = 4
+set.smarttab = true
+
 vim.g.autoformat = false
