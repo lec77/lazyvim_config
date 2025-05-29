9671f3aaac9481c425ebc4d6981ae80351f01a8e
LeChen <lecself@163.com>
2025-05-29 21:16:21 +0800

    feat: add ai assistance

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -3,12 +3,4 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
 
   "nvim-treesitter/nvim-treesitter-context",
-
-  "github/copilot.vim",
-  init = function()
-    vim.g.copilot_filetypes = {
-      ["plaintext"] = false,
-      ["markdown"] = false,
-    }
-  end,
 }
