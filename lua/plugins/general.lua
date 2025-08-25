3b408808d1d525f396664252da7ed9db6648d832
LeChen <lecself@163.com>
2025-08-25 22:19:04 +0800

    money patching bufferline
    Signed-off-by: LeChen <lecself@163.com>

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -34,5 +34,16 @@ return {
         },
       },
     },
-  }
+  },
+
+  -- Disable after update
+  {
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
 }
