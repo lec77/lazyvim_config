cb92c5881f56aa6397e55510986b64fe3d4dcc5e
LeChen <lecself@163.com>
2025-10-21 23:09:36 +0800

    chore: gc

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -1,12 +1,5 @@
 return {
   {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-
-  "nvim-treesitter/nvim-treesitter-context",
-
-  {
     'nvim-treesitter/nvim-treesitter',
     opts = {
       highlight = {
@@ -14,36 +7,5 @@ return {
         disable = { 'markdown' },
       },
     },
-  },
-
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          show_hidden_count = true,
-          hide_dotfiles = false,
-          hide_gitignored = false,
-          hide_by_name = {
-            -- '.git',
-            -- '.DS_Store',
-            -- 'thumbs.db',
-          },
-          never_show = {},
-        },
-      },
-    },
-  },
-
-  -- Disable after update
-  {
-    "akinsho/bufferline.nvim",
-    init = function()
-      local bufline = require("catppuccin.groups.integrations.bufferline")
-      function bufline.get()
-        return bufline.get_theme()
-      end
-    end,
   },
 }
