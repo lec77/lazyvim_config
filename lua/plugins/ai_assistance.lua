4778185bfa0263fa30d94cf89aa19497e7473f43
lec <cen-le@sjtu.edu.cn>
2026-07-06 06:32:24 -0700

    disable copilot

--- lua/plugins/ai_assistance.lua
+++ lua/plugins/ai_assistance.lua
@@ -1,31 +1,22 @@
 return {
   -- {
-  --   "github/copilot.vim",
-  --   init = function()
-  --     vim.g.copilot_filetypes = {
-  --       ["plaintext"] = false,
-  --       ["markdown"] = false,
-  --     }
-  --   end,
+  -- --   "zbirenbaum/copilot.lua",
+  -- --   cmd = "Copilot",
+  -- --   event = "InsertEnter",
+  -- --   config = function()
+  -- --     require("copilot").setup({
+  -- --       suggestion = {
+  -- --         enabled = true,
+  -- --         auto_trigger = true,
+  -- --         keymap = {
+  -- --           accept = "<Tab>",
+  -- --           next = "<C-l>",
+  -- --           prev = false,
+  -- --           dismiss = false,
+  -- --         },
+  -- --       },
+  -- --       panel = { enabled = false },
+  -- --     })
+  -- --   end,
   -- },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          keymap = {
-            accept = "<Tab>",
-            next = "<C-l>",
-            prev = false,
-            dismiss = false,
-          },
-        },
-        panel = { enabled = false },
-      })
-    end,
-  },
 }
